3069867f8a215c26b96fe752a3e0829e9af57676
linnefromice <linnefromice@gmail.com>
2020-07-04 01:40:34 +0900

    update blog project - add library (styled component) & use styled component

--- gatsby_one/src/pages/index.js
+++ gatsby_one/src/pages/index.js
@@ -6,14 +6,12 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
-import indexStyles from "./index.module.css"
-/*
+// import indexStyles from "./index.module.css"
 import styled from "styled-components"
 const GridContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
 `
-*/
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
@@ -23,34 +21,34 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
-      <div className={indexStyles.gridContainer}>
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
-        return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>{` `}<small>{`[${node.frontmatter.tags}]`}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
-          </article>
-        )
-      })}
-      </div>
+      <GridContainer>
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <article key={node.fields.slug}>
+              <header>
+                <h3
+                  style={{
+                    marginBottom: rhythm(1 / 4),
+                  }}
+                >
+                  <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                    {title}
+                  </Link>
+                </h3>
+                <small>{node.frontmatter.date}</small>{` `}<small>{`[${node.frontmatter.tags}]`}</small>
+              </header>
+              <section>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+              </section>
+            </article>
+          )
+        })}
+      </GridContainer>
     </Layout>
   )
 }
